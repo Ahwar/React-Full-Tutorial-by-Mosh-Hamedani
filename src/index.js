50751171fb8bdd8171ce743962885ce07e135c30
Muhammad Ahwar <pakleo123@gmail.com>
2019-05-25 20:39:26 +0500

    24--- Your First React Component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const element = <h1>Hello World</h1>;
-ReactDOM.render(element, document.getElementById("root"));
 import "bootstrap/dist/css/bootstrap.css";
+
+import Counter from "./components/counter";
+
+ReactDOM.render(<Counter />, document.getElementById("root"));
