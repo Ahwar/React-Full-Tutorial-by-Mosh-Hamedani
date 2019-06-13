157cf2fe6f803a45376e531fe2b7f4a556d473fd
Muhammad Ahwar <pakleo123@gmail.com>
2019-06-13 18:23:07 +0500

    38--- Building the Movies Component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -2,14 +2,44 @@ import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService"; // importing data of movies
 
 class Movies extends Component {
-  state = {};
+  state = {
+    movies: getMovies()
+  };
+
   render() {
     return (
       <React.Fragment>
         <h1>Movies</h1>
-        <table />
+        <table className="table">
+          {/* { table heading} */}
+          <thead>
+            <tr>
+              <th>name</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+            </tr>
+          </thead>
+          {/* table body */}
+          <tbody>{this.renderMoviesInTable()}</tbody>
+        </table>
       </React.Fragment>
     );
+  }
+
+  // function to render all movies data in the form of table rows
+  renderMoviesInTable() {
+    return this.state.movies.map(movie => (
+      <tr key={movie._id}>
+        <td>{movie.title}</td>
+        <td>{movie.genre.name}</td>
+        <td>{movie.dailyRentalRate}</td>
+        <td>{movie.numberInStock}</td>
+        <td>
+          <button className="btn btn-danger m-2">Delete</button>
+        </td>
+      </tr>
+    ));
   }
 }
 
