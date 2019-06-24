963dd81aaaac1cb0f427d29adb1792a83003d4b4
Muhammad Ahwar <pakleo123@gmail.com>
2019-06-24 19:32:27 +0500

    52--- Multiple Components in Sync

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Counters from "./components/counters";
+import NavBar from "./components/navBar";
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Counters />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
