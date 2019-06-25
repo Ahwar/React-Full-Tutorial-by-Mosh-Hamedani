99ddd2e1603fb232373ffa06504f6e4637423dbb
Muhammad Ahwar <pakleo123@gmail.com>
2019-06-25 13:57:04 +0500

    53--- Lifting the State Up

--- src/App.js
+++ src/App.js
@@ -2,16 +2,52 @@ import React, { Component } from "react";
 import Counters from "./components/counters";
 import NavBar from "./components/navBar";
 class App extends Component {
+  state = {
+    counters: [
+      // array to hold all counter's objects
+      { id: 1, value: 1 },
+      { id: 2, value: 4 },
+      { id: 3, value: 2 },
+      { id: 4, value: 0 }
+    ]
+  };
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar count={this.state.counters.filter(c => c.value > 0).length} />
         <main className="container">
-          <Counters />
+          <Counters
+            counters={this.state.counters}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            onReset={this.resetValues}
+          />
         </main>
       </React.Fragment>
     );
   }
+  handleDelete = counterId => {
+    let filteredCounters = this.state.counters.filter(
+      // return all the counters array except that counter whoes id is passed to handleMovieDelete method
+      counter => counter.id !== counterId
+    );
+    this.setState({ counters: filteredCounters }); // set all counters list to state after the counter is removed from list
+  };
+
+  handleIncrement = counter => {
+    let counters = [...this.state.counters];
+    let index = counters.indexOf(counter);
+    counters[index].value = counters[index].value + 1;
+    this.setState({ counters: counters });
+  };
+
+  resetValues = () => {
+    let newCounters = this.state.counters.map(counter => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({ counters: newCounters });
+  };
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,58 +2,28 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      // array to hold all counter's objects
-      { id: 1, value: 1 },
-      { id: 2, value: 4 },
-      { id: 3, value: 2 },
-      { id: 4, value: 0 }
-    ]
-  };
   render() {
     return (
       <div>
         <button
-          onClick={this.resetValues}
+          onClick={this.props.onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.state.counters.map(counter => {
+        {this.props.counters.map(counter => {
           return (
             <Counter
               key={counter.id}
               value={counter.value}
-              onDelete={() => this.handleDelete(counter.id)}
-              onIncrement={() => this.handleIncrement(counter)}
+              onDelete={() => this.props.onDelete(counter.id)}
+              onIncrement={() => this.props.onIncrement(counter)}
             />
           );
         })}
       </div>
     );
   }
-  handleDelete = counterId => {
-    let filteredCounters = this.state.counters.filter(
-      // return all the counters array except that counter whoes id is passed to handleMovieDelete method
-      counter => counter.id !== counterId
-    );
-    this.setState({ counters: filteredCounters }); // set all counters list to state after the counter is removed from list
-  };
-  resetValues = () => {
-    let newCounters = this.state.counters.map(counter => {
-      counter.value = 0;
-      return counter;
-    });
-    this.setState({ counters: newCounters });
-  };
-
-  handleIncrement = counter => {
-    let counters = [...this.state.counters];
-    let index = counters.indexOf(counter);
-    counters[index].value = counters[index].value + 1;
-    this.setState({ counters: counters });
-  };
 }
 
 export default Counters;
